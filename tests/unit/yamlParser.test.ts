841df68c9debefd7b7761ad991d203321ddebb2e
eFAILution <eFAILution@users.noreply.github.com>
2026-07-27 13:33:47 -0400

    revert: restore unrelated test spelling changed by pre-commit hook

--- tests/unit/yamlParser.test.ts
+++ tests/unit/yamlParser.test.ts
@@ -38,7 +38,7 @@ include:
     assert.deepStrictEqual(docs, []);
   });
 
-  test('returns [] on unparsable input', () => {
+  test('returns [] on unparseable input', () => {
     assert.deepStrictEqual(parseYamlDocuments('key: "unterminated', true), []);
   });
 });
